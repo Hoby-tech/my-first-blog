23d7637a9a9b682ccd022467a34a041559a3fd1b
Ranja <ranjarivelo@gmail.com>
2019-08-17 15:25:20 +0200

    My django Girls app, first commit

---
+++ scripts.py
@@ -0,0 +1,20 @@
+print("Tongasoa")
+age=18
+if age<=15:
+    print("enfant")
+elif age>15 and age<=25:
+    print("jeune")
+elif age>25 and age<=50:
+    print("adulte") 
+else:
+    print("vieux")
+matières=5
+if matières<=2:
+    print("insuffisant")
+elif 2>matières>=6:
+    print("moyen")
+def bonjour(name):
+    print("bonjour"+" "+name)
+
+
+bonjour("Ranja")
